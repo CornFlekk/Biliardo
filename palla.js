682e0c0daa6ee27bcdd56778fe33887262e45217
CornFlaek <luca.spary04@gmail.com>
2022-03-23 22:28:09 +0100

    aggiunta la classe palla per la gestione di tale oggetto

---
+++ palla.js
@@ -0,0 +1,13 @@
+const ORIGINE_PALLA = new Vector2(12, 12);
+
+function Palla(posizione) {
+	this.posizione = posizione;
+}
+
+Palla.prototype.update = function () {
+
+}
+
+Palla.prototype.draw = function () {
+	Sfondo.drawImage(sprites.whiteBall, this.posizione, ORIGINE_PALLA);
+}
